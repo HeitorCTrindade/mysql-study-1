95bd6cf8e21a1430404106a55ef995772c8cb856
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-10-19 17:06:33 -0300

    Requisitos 15, 16, 17, 18 e 19 finalizados.

--- desafio17.sql
+++ desafio17.sql
@@ -1,1 +1,5 @@
-SELECT supplier_id FROM northwind.purchase_orders WHERE supplier_id = 1 OR supplier_id = 3;
+SELECT id, supplier_id FROM northwind.purchase_orders
+  WHERE supplier_id = 1 
+  OR supplier_id = 3
+  OR supplier_id = 5
+  OR supplier_id = 7;
