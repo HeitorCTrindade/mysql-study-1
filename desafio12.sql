5d19ddf7a712c062e85ab4c8bea5278b7ebbd07c
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-10-19 16:45:41 -0300

    Requisito 12 refatorado com uma requisição que faz mais sentido para buscar pela data.

--- desafio12.sql
+++ desafio12.sql
@@ -1,1 +1,1 @@
-SELECT submitted_date FROM northwind.purchase_orders WHERE submitted_date between '2006-04-26' and '2006-04-27';
+SELECT submitted_date FROM northwind.purchase_orders WHERE DATE(submitted_date) = '2006-04-26';
